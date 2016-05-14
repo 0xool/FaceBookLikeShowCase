40b283ff2f2122a373637d7dc140a26ef6960a46
cyrus refahi <CyrusRefahi@cyruss-macbook-pro.local>
2016-05-14 11:31:53 +0430

    Initial Commit

---
+++ FaceBookLike-ShowCase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceBookLike-ShowCase
+//
+//  Created by CyrusREfahi on 2/25/1395 AP.
+//  Copyright © 1395 cyrus refahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceBookLike-ShowCase/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FaceBookLike-ShowCase
+//
+//  Created by CyrusREfahi on 2/25/1395 AP.
+//  Copyright © 1395 cyrus refahi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
